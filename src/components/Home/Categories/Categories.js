d1a033097af0b03e08984054711dc15e1a9c0ec8
mdmahfujarrahmane <ahmedmahfuj28@gmail.comm>
2022-06-14 16:16:12 +0600

    category section added

---
+++ src/components/Home/Categories/Categories.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useQuery } from 'react-query';
+import Category from '../Category/Category';
+
+const Categories = () => {
+
+    const { data: categories, isLoading } = useQuery("categories", () =>
+        fetch("http://localhost:5000/categories").then(res => res.json())
+    );
+
+    console.log(categories);
+    return (
+        <section className="container mx-auto">
+            <h2 className="text-4xl text-accent my-14 text-center lg:text-left">CATEGORIES</h2>
+            <div className="grid lg:grid-cols-3 place-content-center my-20">
+                {categories.map((category) => (
+                    <Category key={category._id} category={category} />
+                ))}
+            </div>
+        </section>
+    );
+};
+
+export default Categories;
--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Categories from '../Categories/Categories';
 
 const Home = () => {
     return (
         <section className="bg-home-bg bg-cover bg-no-repeat">
             <Banner/>
+            <Categories/>
         </section>
     );
 };
